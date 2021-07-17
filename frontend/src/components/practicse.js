58e4d37291d2e695659d0d41c0a022baa18fd5f3
Manali Gupta <guptamanali0@gmail.com>
2021-07-17 12:16:36 +0530

    Save to Github

--- frontend/src/components/practicse.js
+++ frontend/src/components/practicse.js
@@ -1,42 +1,104 @@
-import ReactRanger from 'react-ranger'
- 
-function App() {
-  const [values, setValues] = React.useState([10])
- 
-  const { getTrackProps, handles } = useRanger({
-    values,
-    onChange: setValues,
-    min: 0,
-    max: 100,
-    stepSize: 5,
-  })
- 
-  return (
-    <>
-      <div
-        {...getTrackProps({
-          style: {
-            height: '4px',
-            background: '#ddd',
-            boxShadow: 'inset 0 1px 2px rgba(0,0,0,.6)',
-            borderRadius: '2px',
-          },
-        })}
-      >
-        {handles.map(({ getHandleProps }) => (
-          <div
-            {...getHandleProps({
-              style: {
-                width: '12px',
-                height: '12px',
-                borderRadius: '100%',
-                background: 'linear-gradient(to bottom, #eee 45%, #ddd 55%)',
-                border: 'solid 1px #888',
-              },
-            })}
-          />
-        ))}
-      </div>
-    </>
-  )
+import React, { Component } from 'react';
+import {BrowserRouter , Route} from 'react-router-dom'
+import axios from 'axios';
+import {Link,NavLink } from 'react-router-dom';
+import './../styles/index.css'
+import image12 from './../images/ishop.png'
+import image2 from './../images/i2.png'
+import image13 from './../images/i13.png'
+
+
+
+
+class Accs extends Component{
+
+  state={
+    menu:[],
+    product:[],
+    product1:[]
+  
+  }
+
+  myFunctionA = () => {
+    this.setState((state) => (this.product=this.product1,{product:state.product.filter((item) => item.color === "Black")}))
+    console.log("yes")
 }
+
+myFunctionB = () => {
+  this.setState((state) => (this.product=this.product1,{product:state.product.filter((item) => item.category === "Wireless & Air ports")}))
+  console.log("yes")
+}
+
+
+
+  componentDidMount() {
+     let one ="http://localhost:3001/home";
+    let two="http://localhost:3001/products";
+
+const requestOne = axios.get(one);
+const requestTwo = axios.get(two);
+
+axios.all([requestOne, requestTwo])
+.then(
+axios.spread((...responses) => {
+    const responseOne = responses[0].data;
+    const responseTwo = responses[1].data;
+    console.log(responseOne, responseTwo)
+
+    this.setState({
+       menu:responseOne,
+       product:responseTwo,
+       product1:responseTwo
+      })
+  })
+)
+     .catch((error)=>
+       console.log(error+"Wrong")    
+     
+     )
+     console.log("fetching data")
+ 
+   }
+  
+
+    render(){
+      return(
+        <>
+           <div className="one"><img src={image12}/></div>
+           <div className="top">
+           {this.state.menu.map((i)=>(
+               <div key={i.id}>
+                 <NavLink  id="link"  to="/sell">{i.item}</NavLink>
+               </div> 
+))}
+ </div>
+<div className="toptwo">
+ <NavLink activeClassName="active" to="/" id="linkone">Store</NavLink>
+ <NavLink activeClassName="active" to="#" id="linkone"> / </NavLink>
+<NavLink activeClassName="active" to="/" id="linkone">Accessories</NavLink>
+</div>
+
+<img src={image13} className="image13"/>
+<button onClick={this.myFunctionA}>Click me to display item 1 & 4</button>
+<button onClick={this.myFunctionB}>Click me to display</button>
+
+
+
+
+
+{this.state.product.map((i)=>(
+               <div key={i.id}>
+                 <NavLink  id="link"  to="/sell">{i.item}</NavLink>
+                 <img id="link"   src={i.path} />
+               </div> 
+))}
+
+
+
+  </>
+      )
+      }
+}
+
+
+export default Accs;
